93fffa261bc89585ded244c69d98d3de73f99a7d
sinooi <wongsinooi@gmail.com>
2025-04-10 14:41:18 +0800

    test on SCM polling

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -10,7 +10,7 @@ public class App {
     public App() {}
 
     public static void main(String[] args) {
-        System.out.println("Testing on pipeline");
+        System.out.println("Testing on SCM Polling");
     }
 
     public String getMessage() {
