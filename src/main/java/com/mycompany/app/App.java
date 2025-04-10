834f53d79293aee2b05a80a5cbbee45c075f595c
sinooi <wongsinooi@gmail.com>
2025-04-10 14:24:10 +0800

    test on pipeline

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -10,7 +10,7 @@ public class App {
     public App() {}
 
     public static void main(String[] args) {
-        System.out.println("Testing the periodic build (webhook)");
+        System.out.println("Testing on pipeline");
     }
 
     public String getMessage() {
